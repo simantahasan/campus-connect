c068d438b854c7c380d7f886f71059df4ca6873e
Simanta Hasan Kollol <simanta.hasan.kollol@g.bracu.ac.bd>
2026-01-01 23:47:14 +0600

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -5,5 +5,5 @@ const isLocalhost = window.location.hostname === "localhost" ||
                     window.location.hostname === "127.0.0.1";
 
 export const API_URL = isLocalhost 
-  ? "http://localhost:5000"                      // Use this when on your laptop
+  ? "https://campus-connect-347l.onrender.com"                      // Use this when on your laptop
   : `http://${window.location.hostname}:5000`;   // Use this when on your phone
